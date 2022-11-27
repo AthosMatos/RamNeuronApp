333dea8099aa20c9d1ba5c07a8153604d43aeeda
Athos Matos <ladiesman217.as@gmail.com>
2022-11-27 01:08:33 -0300

    improving

--- src/App.js
+++ src/App.js
@@ -1,19 +1,23 @@
 import React, { useEffect, useRef, useState } from "react";
 
+
 function App() 
 {
   const [canvas,setCanvas] = useState()
   const [canvasGrid,setcanvasGrid] = useState()
   const [segAmount,setsegAmount] = useState(4)
   const [TrainedRams,setTrainedRams] = useState(0)
+  const [segCanvasCtx,setSegCanvasCtx] = useState()
+  const [gridCanvasCtx,setGridCanvasCtxCanvasCtx] = useState()
 
   const canvasRef = useRef()
   const canvasGridRef = useRef()
-  const DrawlineWidth = 6;
-  var showGrid = true
-  var SegmentationSize = 4
-  var isPainting = false;
-  var TrainedRamNeurons = []
+  const DrawlineWidth = 10;
+
+  let showGrid = true
+  let SegmentationSize = 4
+  let isPainting = false;
+  let TrainedRamNeurons = []
 
   useEffect(()=>
   {
@@ -28,147 +32,175 @@ function App()
   {
     if(canvas && canvasGrid)
     {
-      SegmentationAnalysis()
-      GridRender()
+      setSegCanvasCtx(canvas.getContext("2d",{willReadFrequently: true}))
+      setGridCanvasCtxCanvasCtx(canvasGrid.getContext("2d",{willReadFrequently: true}))
     }
   },[canvasGrid,canvas])
 
+  useEffect(()=>
+  {
+    if(segCanvasCtx && gridCanvasCtx)
+    {
+      SegmentationAnalysis()
+      GridRender()
+    }
+  },[segCanvasCtx,gridCanvasCtx])
+
+  function CleanCanvas()
+  {
+    const [w, h] = [canvas.width, canvas.height];
+
+    segCanvasCtx.clearRect(0, 0, w, h);  
+    segCanvasCtx.fillRect(0, 0, w, h);
+  }
+
+  function VerificarImg(ImgMatrix)
+  {
+    let Equals = 0
+    for(let i=0;i<TrainedRamNeurons.length;i++)
+    {
+      let completelyEqual = true
+      for(let i2 = 0;i2<TrainedRamNeurons[i].length;i2++)
+      {
+        for(let i3 = 0;i3<TrainedRamNeurons[i][i2].length;i3++)
+        {
+          if(TrainedRamNeurons[i][i2][i3] != ImgMatrix[i2][i3])
+          {
+            completelyEqual = false
+            break
+          }
+        }
+      }
+      if(completelyEqual)
+      {
+        Equals++
+      }
+    }
+    return `${(Equals/(TrainedRamNeurons.length))*100}%`
+  }
+
+  function AnalyzeImg(trainedDataSet)
+  {
+    const [w, h] = [canvas.width, canvas.height];
+
+    const isBackGround = (pixel) =>
+    {
+      //0.1
+      var greyColor = 220
+      if(trainedDataSet)greyColor=255
+      
+      for(let i=0;i<pixel.data.length-1;i++)
+      {
+        //console.log(pixel.data[i])
+        if(pixel.data[i] != greyColor) return false
+      }
+      return true
+    }
+
+    let RamMatrix = []
+    let segH = h/SegmentationSize
+    let prevsegH = 0
+
+    for(let iX = 0;iX<SegmentationSize;iX++)
+    {
+      let prevsegW = 0
+      let segW = w/SegmentationSize
+      let RamMatrix_X = []
+      //console.log(`analizing from ${prevsegH}px to ${segH}px Vertically`)
+
+      for(let iY=0;iY<SegmentationSize;iY++)
+      {
+        let segmentIsUsed = false
+        for(let x = prevsegW;x<segW; x++)
+        {
+          for(let y = prevsegH;y<segH;y++)
+          {
+            const pixel = segCanvasCtx.getImageData(x,y,1,1)
+            //console.log(pixel)
+            if(!isBackGround(pixel)) segmentIsUsed = true
+          }
+        }
+        RamMatrix_X.push(segmentIsUsed?1:0)
+        //console.log(segmentIsUsed)
+        //console.log(`analizing from ${prevsegW}px to ${segW}px Horizontaly`)
+        prevsegW = segW
+        segW += w/SegmentationSize
+      }
+      RamMatrix.push(RamMatrix_X)
+      prevsegH = segH
+      segH += h/SegmentationSize
+    }
+
+    //console.log(RamMatrix)
+
+    return RamMatrix
+  }
+
+
+	const TrainFromFiles = (event) => 
+  {
+    //console.log(event.target.files)
+		for(let i=0;i<event.target.files.length;i++)
+    {
+      var reader = new FileReader();
+      reader.onload = function(event)
+      {
+        var img = new Image();
+        img.onload = function()
+        {
+          CleanCanvas()
+          segCanvasCtx.drawImage(img,0,0,canvas.width,canvas.height);
+          TrainedRamNeurons.push(AnalyzeImg(true))
+          setTrainedRams(TrainedRamNeurons.length)
+          
+        }
+        img.src = event.target.result;
+      }
+      reader.readAsDataURL(event.target.files[i]);  
+
+      
+    }
+	};
 
   const SegmentationAnalysis = () =>
   {
     const [w, h] = [canvas.width, canvas.height];
-    const ctx = canvas.getContext("2d",{willReadFrequently: true});
+    
     const canvasOffsetX = canvas.offsetLeft;
     const canvasOffsetY = canvas.offsetTop;
-    const Clean = document.getElementById('Clean');
     const Train = document.getElementById('Train');
-    const StandardTrain = document.getElementById('StandardTrain');
     const Analyze = document.getElementById('Analyze');
 
-    if (!ctx ) return;
+    if (!segCanvasCtx ) return;
     
-    ctx.fillStyle = 'rgb(220, 220, 220)';
-    ctx.clearRect(0, 0, w, h);   
-    ctx.fillRect(0, 0, w, h);
-    ctx.strokeStyle = 'rgba(0, 0, 0, 1)';
-
-    function VerificarImg(ImgMatrix)
-    {
-      for(let i=0;i<TrainedRamNeurons.length;i++)
-      {
-        let completelyEqual = true
-        for(let i2 = 0;i2<TrainedRamNeurons[i].length;i2++)
-        {
-          for(let i3 = 0;i3<TrainedRamNeurons[i][i2].length;i3++)
-          {
-            if(TrainedRamNeurons[i][i2][i3] != ImgMatrix[i2][i3])
-            {
-              completelyEqual = false
-              break
-            }
-          }
-        }
-        if(completelyEqual) return true
-      }
-      return false
-    }
-
-    function AnalyzeImg()
-    {
-      const isBackGround = (pixel) =>
-      {
-        //0.1
-        const greyColor = 220
-        for(let i=0;i<pixel.data.length-1;i++)
-        {
-          //console.log(pixel.data[i])
-          if(pixel.data[i] != greyColor) return false
-        }
-        return true
-      }
-
-      let RamMatrix = []
-      let segH = h/SegmentationSize
-      let prevsegH = 0
-  
-      for(let iX = 0;iX<SegmentationSize;iX++)
-      {
-        let prevsegW = 0
-        let segW = w/SegmentationSize
-        let RamMatrix_X = []
-        //console.log(`analizing from ${prevsegH}px to ${segH}px Vertically`)
-
-        for(let iY=0;iY<SegmentationSize;iY++)
-        {
-          let segmentIsUsed = false
-          for(let x = prevsegW;x<segW; x++)
-          {
-            for(let y = prevsegH;y<segH;y++)
-            {
-              const pixel = ctx.getImageData(x,y,1,1)
-              //console.log(pixel)
-              if(!isBackGround(pixel)) segmentIsUsed = true
-            }
-          }
-          RamMatrix_X.push(segmentIsUsed?1:0)
-          //console.log(segmentIsUsed)
-          //console.log(`analizing from ${prevsegW}px to ${segW}px Horizontaly`)
-          prevsegW = segW
-          segW += w/SegmentationSize
-        }
-        RamMatrix.push(RamMatrix_X)
-        prevsegH = segH
-        segH += h/SegmentationSize
-      }
-
-      return RamMatrix
-    }
+    segCanvasCtx.fillStyle = 'rgb(220, 220, 220)';
+    segCanvasCtx.clearRect(0, 0, w, h);   
+    segCanvasCtx.fillRect(0, 0, w, h);
+    segCanvasCtx.strokeStyle = 'rgba(0, 0, 0, 1)';
 
     canvasGrid.addEventListener('mousedown', (e) => 
     {
         isPainting = true;
-        ctx.moveTo(e.clientX - canvasOffsetX , e.clientY - canvasOffsetY);
+        segCanvasCtx.moveTo(e.clientX - canvasOffsetX , e.clientY - canvasOffsetY);
     });
 
     canvasGrid.addEventListener('mouseup', e => 
     {
         isPainting = false;
-        ctx.stroke();
-        ctx.beginPath();
+        segCanvasCtx.stroke();
+        segCanvasCtx.beginPath();
     });
 
     canvasGrid.addEventListener('mousemove', (e) => 
     {
         if(!isPainting) return;
         
-        ctx.lineWidth = DrawlineWidth;
-        ctx.lineCap = 'round';
-        ctx.lineTo(e.clientX - canvasOffsetX , e.clientY - canvasOffsetY);
-        ctx.stroke();
+        segCanvasCtx.lineWidth = DrawlineWidth;
+        segCanvasCtx.lineCap = 'round';
+        segCanvasCtx.lineTo(e.clientX - canvasOffsetX , e.clientY - canvasOffsetY);
+        segCanvasCtx.stroke();
     });
-    Clean.addEventListener('click', (e) => 
-    {
-      ctx.clearRect(0, 0, w, h);  
-      ctx.fillRect(0, 0, w, h);
-    });
-    StandardTrain.addEventListener('click', (e) => 
-    {
-      
-      //console.log(ctx)
-      //TrainedRamNeurons.push(AnalyzeImg())
-      //setTrainedRams(TrainedRamNeurons.length)
-
-      //console.log(TrainedRamNeurons)'
-
-      const img = new Image();
-      img.onload = () => 
-      {
-        
-        ctx.drawImage(img, 0, 0,w,h);
-      }
-      img.src = require('./test.png');
-      
-    });
+   
     Train.addEventListener('click', (e) => 
     {
       //console.log(ctx)
@@ -180,7 +212,7 @@ function App()
     });
     Analyze.addEventListener('click', (e) => 
     {
-      console.log(AnalyzeImg())
+      //console.log(AnalyzeImg())
       console.log(VerificarImg(AnalyzeImg()))
     });
 
@@ -189,8 +221,8 @@ function App()
   const GridRender = () =>
   {
     const [w, h] = [canvasGrid.width, canvasGrid.height];
-    const ctx = canvasGrid.getContext("2d",{willReadFrequently: true});
-    if (!ctx ) return;
+    
+    if (!gridCanvasCtx ) return;
 
     const RaiseSegmentations = document.getElementById('RaiseSegmentations');
     const ResetSegmentations = document.getElementById('ResetSegmentations');
@@ -198,30 +230,30 @@ function App()
     const ShowGrid = document.getElementById('ShowGrid');
    
     //ctx.fillStyle = 'rgb(200, 200, 200)';
-    ctx.lineWidth = 0.5;
-    ctx.strokeStyle = 'rgb(0, 0, 0)';
+    gridCanvasCtx.lineWidth = 0.5;
+    gridCanvasCtx.strokeStyle = 'rgb(0, 0, 0)';
 
     function drawGrid(notShow)
     {
-      ctx.clearRect(0, 0, w, h);   
+      gridCanvasCtx.clearRect(0, 0, w, h);   
       //ctx.fillRect(0, 0, w, h);
       if(!notShow)
       {
-        ctx.beginPath();
+        gridCanvasCtx.beginPath();
     
         for(let i=0,segX = 0,segY = 0;i<SegmentationSize + 1;i++)
         {
-          ctx.moveTo(segX, 0);
-          ctx.lineTo(segX, h);
+          gridCanvasCtx.moveTo(segX, 0);
+          gridCanvasCtx.lineTo(segX, h);
 
           segX+=w/SegmentationSize
 
-          ctx.moveTo(0, segY);
-          ctx.lineTo(w, segY);
+          gridCanvasCtx.moveTo(0, segY);
+          gridCanvasCtx.lineTo(w, segY);
 
           segY+=h/SegmentationSize
         }
-        ctx.stroke();
+        gridCanvasCtx.stroke();
         }
     }
     drawGrid()
@@ -298,20 +330,32 @@ function App()
         <button id="ShowGrid" >
           <p>Toogle Grid</p>
         </button>
-        <button id="Clean" >
+        <button id="Clean" onClick={CleanCanvas}>
           <p>Limpar</p>
         </button>
         <button id="Train" >
           <p>Treinar Novo</p>
         </button>
-        <button id="StandardTrain" >
-          <p>Treino padrao</p>
-        </button>
       </div>
-      <div style={{position:'absolute',marginTop:485}}>
+      
+      <div style={{alignItems:'center',position:'absolute',marginTop:485,display:'flex'}}>
         <button id="Analyze" >
           <p>Analisar</p>
         </button>
+
+        <button style=
+        {{
+          width:100,
+          alignItems:'center',
+          display:'flex',
+          flexDirection:'column',
+          justifyContent:'center',
+        
+        }}>
+          <label htmlFor="files" >Selecionar imagens para analizar</label>
+          <input id="files" style={{visibility:'hidden'}} type="file" multiple onChange={TrainFromFiles}/>
+        </button>
+
       </div>
     </div>
   );
