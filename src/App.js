7a4214df00233cc31926c21d22b856945c0504ca
Athos Matos <ladiesman217.as@gmail.com>
2022-11-30 23:47:06 -0300

    adding helper canvas to see how the neuron its been trained

--- src/App.js
+++ src/App.js
@@ -5,24 +5,34 @@ function App()
 {
   const [canvas,setCanvas] = useState()
   const [canvasGrid,setcanvasGrid] = useState()
+  const [canvasComputingGrid,setcanvasComputingGrid] = useState()
+
   const [segAmount,setsegAmount] = useState(4)
   const [TrainedTuplas,setTrainedTuplas] = useState(0)
+
   const [segCanvasCtx,setSegCanvasCtx] = useState()
   const [gridCanvasCtx,setGridCanvasCtxCanvasCtx] = useState()
+  const [canvasComputingGridCtx,setcanvasComputingGridCtx] = useState()
+
   const [NeuronTrainPorcentage,setNeuronTrainPorcentage] = useState(0)
   const [NeuronTrainAmount,setNeuronTrainAmount] = useState(0)
+  const [currentLetter,setCurrentLetter] = useState('A')
 
   const canvasRef = useRef()
   const canvasGridRef = useRef()
+  const canvasAsSeeRef = useRef()
+
   const DrawlineWidth = 10;
 
   let showGrid = true
   let SegmentationSize = 4
   let isPainting = false;
   let Discriminators = []
+  let lettersData = []
   let nextSegH = undefined
   let intervalMS = 60
   let NeuronIndex = 0
+  let currentButton = 'A'
 
   useEffect(()=>
   {
@@ -30,26 +40,28 @@ function App()
     {
       setCanvas(canvasRef.current)
       setcanvasGrid(canvasGridRef.current)
+      setcanvasComputingGrid(canvasAsSeeRef.current)
     }
-  },[canvasGridRef.current,canvasRef.current])
+  },[canvasGridRef.current,canvasRef.current,canvasAsSeeRef.current])
 
   useEffect(()=>
   {
-    if(canvas && canvasGrid)
+    if(canvas && canvasGrid && canvasComputingGrid)
     {
       setSegCanvasCtx(canvas.getContext("2d",{willReadFrequently: true}))
       setGridCanvasCtxCanvasCtx(canvasGrid.getContext("2d",{willReadFrequently: true}))
+      setcanvasComputingGridCtx(canvasComputingGrid.getContext("2d",{willReadFrequently: true}))
     }
-  },[canvasGrid,canvas])
+  },[canvasGrid,canvas,canvasComputingGrid])
 
   useEffect(()=>
   {
-    if(segCanvasCtx && gridCanvasCtx)
+    if(segCanvasCtx && gridCanvasCtx && canvasComputingGridCtx)
     {
       SegmentationAnalysis()
       GridRender()
     }
-  },[segCanvasCtx,gridCanvasCtx])
+  },[segCanvasCtx,gridCanvasCtx,canvasComputingGridCtx])
 
   function CleanCanvas()
   {
@@ -65,7 +77,7 @@ function App()
 
     if(NeuronIndex == Discriminators.length)NeuronIndex=0
     //console.log('Discriminators',Discriminators[NeuronIndex])
-    console.log('ImgMatrix',ImgMatrix)
+    //console.log('ImgMatrix',ImgMatrix)
    
     for(let i = 0;i<Discriminators[NeuronIndex].length;i++)
     {
@@ -75,10 +87,8 @@ function App()
         if(Discriminators[NeuronIndex][i][i2] != ImgMatrix[i2]) 
         {
           completelyEqual = false
-          
           break
         }
-  
         if(completelyEqual) Equals++
       }
     }
@@ -176,6 +186,65 @@ function App()
     return RamMatrix_X
   }
 
+  function AnalyzeFullImg(trainedDataSet)
+  {
+    const [w, h] = [canvas.width, canvas.height];
+
+    const isBackGround = (pixel) =>
+    {
+      //0.1
+      let greyColor = 220
+      if(trainedDataSet)greyColor=255
+      
+      for(let i=0;i<pixel.data.length-1;i++)
+      {
+        //console.log(pixel.data[0])
+        if((pixel.data[i] != greyColor)) return false
+      }
+      return true
+    }
+
+    let prevsegH = 0
+    let segH = h/SegmentationSize
+    let RamMatrix = []
+    //console.log(`analizing from ${prevsegH}px to ${segH}px Vertically`)
+
+    for(let iY=0;iY<SegmentationSize;iY++)
+    {
+      let RamMatrix_X = []
+      let prevsegW = 0
+      let segW = w/SegmentationSize
+      for(let iX=0;iX<SegmentationSize;iX++)
+      {
+        let segmentIsUsed = false
+        for(let x = prevsegW;x<segW; x++)
+        {
+          for(let y = prevsegH;y<segH;y++)
+          {
+            const pixel = segCanvasCtx.getImageData(x,y,1,1)
+            //console.log(pixel)
+            if(!isBackGround(pixel)) segmentIsUsed = true
+          }
+        }
+        RamMatrix_X.push(segmentIsUsed?1:0)
+        //console.log(segmentIsUsed)
+        //console.log(`analizing from ${prevsegW}px to ${segW}px Horizontaly`)
+        //console.log('iX',iX)
+
+        prevsegW = segW
+        segW += w/SegmentationSize
+      }
+      RamMatrix.push(RamMatrix_X)
+
+      prevsegH = segH
+      segH += h/SegmentationSize
+    }
+  
+    //console.log(RamMatrix_X)
+
+    return RamMatrix
+  }
+
 	const TrainFromFiles = (event) => 
   {
     //console.log(event.target.files)
@@ -208,10 +277,51 @@ function App()
     const AnalyzeAll = document.getElementById('AnalyzeAll');
     //const RaiseInterval = document.getElementById('RaiseInterval');
     //const LowerInterval = document.getElementById('LowerInterval');
-   
+    const A = document.getElementById('A');
+    const B = document.getElementById('B');
+    const C = document.getElementById('C');
+    const D = document.getElementById('D');
+
+    A.addEventListener('click', (e) => 
+    {
+      if(lettersData['A']) Discriminators = lettersData['A']
+      else Discriminators = []
+
+      currentButton = 'A'
+      setCurrentLetter(currentButton)
+    })
+    B.addEventListener('click', (e) => 
+    {
+      if(lettersData['B']) Discriminators = lettersData['B']
+      else Discriminators = []
+
+      currentButton = 'B'
+      setCurrentLetter(currentButton)
+    })
+    C.addEventListener('click', (e) => 
+    {
+      if(lettersData['C']) Discriminators = lettersData['C']
+      else Discriminators = []
+
+      currentButton = 'C'
+      setCurrentLetter(currentButton)
+    })
+    D.addEventListener('click', (e) => 
+    {
+      if(lettersData['D']) Discriminators = lettersData['D']
+      else Discriminators = []
+
+      currentButton = 'D'
+      setCurrentLetter(currentButton)
+    })
+
     TrainSection.addEventListener('click', (e) => 
     {
       let index = 0
+      //console.log(currentButton)
+
+      ComputingGridRender(AnalyzeFullImg())
+
       const interval = setInterval(() => 
       {
         let RAM
@@ -224,12 +334,12 @@ function App()
         RAM.push(AnalyzeImg(undefined,interval))
         Discriminators[index] = RAM
 
-        console.log(Discriminators)
+        //console.log(Discriminators)
         index++
       },intervalMS)
       
-      //console.log(TrainedRamNeurons)
-
+      lettersData[currentButton] = Discriminators
+      //console.log(lettersData)
       setTrainedTuplas((tt)=>tt+1)
     });
     /*
@@ -241,16 +351,23 @@ function App()
     */
     AnalyzeAll.addEventListener('click', (e) => 
     {
-      console.log(Discriminators)
-      let porcentage = 0
+      //console.log(Discriminators)
+    
+      ComputingGridRender(AnalyzeFullImg())
 
       if(!Discriminators.length) return
-      
-      const interval = setInterval(()=>
+      console.log(Discriminators)
+
+      const interval = setInterval(() => 
       {
-        porcentage+=VerificarImg(AnalyzeImg(undefined,interval))
-        console.log(`${porcentage/SegmentationSize}%`)
+        
+        console.log(VerificarImg(AnalyzeImg(undefined,interval)))
+       
+
+        
+        
       },intervalMS)
+
     })
 
     //RaiseInterval.addEventListener('click', (e) => intervalMS+=50);
@@ -355,12 +472,48 @@ function App()
     });
   }
 
+  const ComputingGridRender = (CurrentDrawAsSegmentation)=>
+  {
+    const [w, h] = [canvasComputingGrid.width, canvasComputingGrid.height];
+    
+    //console.log(CurrentDrawAsSegmentation)
+
+    canvasComputingGridCtx.clearRect(0, 0, w, h);   
+    canvasComputingGridCtx.fillStyle = 'rgb(220, 0, 0)';
+    //gridCanvasCtx.fillRect(0, 0, w, h);
+
+    const squareSize = h/SegmentationSize
+
+    let fromX = 0
+    for(let i = 0; i<SegmentationSize; i++)
+    {
+      let fromY = 0
+      for(let i2 = 0; i2<SegmentationSize; i2++)
+      {
+        if(CurrentDrawAsSegmentation[i][i2] == 1)
+        {
+          //await sleep(1000)
+          canvasComputingGridCtx.fillRect(fromY, fromX, squareSize, squareSize);
+          
+          //console.log(`drawing from X[${i}] Y[${i2}]`)
+          //console.log(`drawing from X[${fromX}] to X[${toX}] from Y[${fromY}] to Y[${toY}]`)
+        }
+        fromY += squareSize
+      }
+      fromX += squareSize
+    }
+  }
+  
   return (
     <div style={{display:'flex', flexDirection:'column',alignItems:'center',justifyContent:"center",minHeight:'100vh',minWidth:'100vw'}}>
       <p style={{position:'absolute',marginBottom:385}}>{`Segmentacoes: ${segAmount}`}</p>
-      
+    
       <canvas style={{border:'solid black 1px'}} width={300} height={300} ref={canvasRef}/> 
-     
+      
+      <div style={{display:'flex',position:'absolute',left:700,top:0,bottom:0,right:0,alignItems:'center',justifyContent:'center'}}>
+        <canvas style={{border:'solid black 1px'}} width={300} height={300} ref={canvasAsSeeRef}/>
+      </div>
+
       <div style={{display:'flex',position:'absolute',left:0,top:0,bottom:0,right:0,alignItems:'center',justifyContent:'center'}}>
         <canvas style={{cursor:'crosshair',}} width={300} height={300} ref={canvasGridRef}/>
       </div>
@@ -414,7 +567,7 @@ function App()
       style=
       {{
         position:'absolute',
-        marginLeft:520,
+        marginRight:760,
         alignItems:'center',
         justifyContent:'space-evenly',
         display:'flex',
@@ -438,8 +591,8 @@ function App()
         justifyContent:'space-between',
         display:'flex',
         flexDirection:'column',
-        marginTop:465,
-        height:120
+        marginTop:505,
+        height:180
       }}>
         {/*
           <button id="AnalyzeLine" >
@@ -460,12 +613,12 @@ function App()
           flexDirection:'row',
           width:340,
         }}>
-          <button style={{width:100}} id="AnalyzeAll" >
-            <p>Analisar</p>
-          </button>
-
           <button style={{width:100}} id="TrainSection" >
             <p>Treinar Novo</p>
+          </button>
+
+          <button style={{width:100}} id="AnalyzeAll" >
+            <p>Analisar</p>
           </button>
 
           <button style=
@@ -481,7 +634,32 @@ function App()
           
           </button>
         </div>
-        
+
+        <div style=
+        {{
+          alignItems:'center',
+          justifyContent:'space-evenly',
+          display:'flex',
+          flexDirection:'row',
+          width:340,
+        }}>
+          <button style={{width:50}} id='A'>
+            <p>A</p>
+          </button>
+
+          <button style={{width:50}} id='B'>
+            <p>B</p>
+          </button>
+
+          <button style={{width:50}} id='C'>
+            <p>C</p>
+          </button>
+
+          <button style={{width:50}} id='D'>
+            <p>D</p>
+          </button>
+        </div>
+        <p style={{margin:0}}>{`Letra Selecionada: ${currentLetter}`}</p>
       </div>
     </div>
   );
